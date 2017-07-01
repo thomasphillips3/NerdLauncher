491319c8f528624e1589b681f1a2c1bd6b90019b
Thomas Phillips <thomasphillips3@gmail.com>
2017-06-30 20:40:50 -0700

    Initial commit
    - Single fragment activity that does nothing so far
    - Prints number of installed apps to the console

---
+++ app/src/main/java/com/thomasphillips3/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,48 @@
+package com.thomasphillips3.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by thomas on 6/23/17.
+ */
+
+public class NerdLauncherFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private final static String TAG = "NerdLauncherFragment";
+
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        setupAdapter();
+        return v;
+    }
+
+    void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "There are " + activities.size() + " activities.");
+    }
+}
